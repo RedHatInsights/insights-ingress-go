4e8bea88e8ec195f10fe41745ec4d15723dc49af
Stephen Adams <tsadams@gmail.com>
2019-05-22 16:51:38 -0400

    Add b64_identity to request and response
    Pup needs this at the very least for inventory posts

--- validators/types.go
+++ validators/types.go
@@ -2,25 +2,27 @@ package validators
 
 // Request is sent to the validation topic for each new payload
 type Request struct {
-	Account   string      `json:"account"`
-	Category  string      `json:"category"`
-	Metadata  interface{} `json:"metadata"`
-	RequestID string      `json:"request_id"`
-	Principal string      `json:"principal"`
-	Service   string      `json:"service"`
-	Size      int64       `json:"size"`
-	URL       string      `json:"url"`
+	Account     string      `json:"account"`
+	Category    string      `json:"category"`
+	Metadata    interface{} `json:"metadata"`
+	RequestID   string      `json:"request_id"`
+	Principal   string      `json:"principal"`
+	Service     string      `json:"service"`
+	Size        int64       `json:"size"`
+	URL         string      `json:"url"`
+	B64Identity string      `json:"b64_identity"`
 }
 
 // Response is returned by validators and sent via the announcement
 type Response struct {
-	Account    string            `json:"account"`
-	Validation string            `json:"validation"`
-	RequestID  string            `json:"request_id"`
-	Principal  string            `json:"principal"`
-	Service    string            `json:"service"`
-	URL        string            `json:"url"`
-	Extras     map[string]string `json:"extras"`
+	Account     string            `json:"account"`
+	Validation  string            `json:"validation"`
+	RequestID   string            `json:"request_id"`
+	Principal   string            `json:"principal"`
+	Service     string            `json:"service"`
+	URL         string            `json:"url"`
+	B64Identity string            `json:"b64_identity"`
+	Extras      map[string]string `json:"extras"`
 }
 
 // Validator validates requests
