050543fa535ea01aedee5b0fcf77d2602cff17d9
Stephen Adams <tsadams@gmail.com>
2019-06-20 14:58:44 -0400

    Create tests for version endpoint
    Former-commit-id: 5a3d1a88d7da5a250c6bd6ac3078babb13e62b80

--- version/version_test.go
+++ version/version_test.go
@@ -3,17 +3,43 @@ package version_test
 import (
 	"github.com/redhatinsights/insights-ingress-go/version"
 
+	"errors"
+	"net/http"
+	"net/http/httptest"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
+func GetServer() (*httptest.ResponseRecorder, *http.Request) {
+	req, err := http.NewRequest("GET", "/version", nil)
+	if err != nil {
+		errors.New("version endpoint failed")
+	}
+
+	rr := httptest.NewRecorder()
+
+	return rr, req
+
+}
+
 var _ = Describe("Version", func() {
 
 	Describe("Reading from the version file", func() {
-		It("Should return a string", func() {
+		It("should return a string", func() {
 			s := version.ReadVersion()
 			Expect(s).NotTo(BeNil())
-			Expect(s).To(Equal("1.0.0"))
+			Expect(s).To(Equal("0.0.0"))
+		})
+	})
+
+	Describe("GET from the version endpoint", func() {
+		It("should return a json doc containg version", func() {
+			rr, req := GetServer()
+			handler := http.HandlerFunc(version.GetVersion)
+			handler.ServeHTTP(rr, req)
+			Expect(rr.Code).To(Equal(http.StatusOK))
+			Expect(rr.Body.String()).To(Equal(`{"version":"0.0.0","commit":"notrunninginopenshift"}`))
 		})
 	})
 })
