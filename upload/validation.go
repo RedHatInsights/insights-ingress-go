9840a731ea210bead36a7c3eab8921bd22d2bd22
Stephen Adams <tsadams@gmail.com>
2019-07-17 14:50:28 -0400

    Add application/gzip to valid content types
    We keep gettign insights payloads with this content type. We need to
    cover it with our service mapper
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- upload/validation.go
+++ upload/validation.go
@@ -1,26 +1,34 @@
 package upload
 
 import (
-	"regexp"
 	"errors"
+	"regexp"
+
 	"github.com/redhatinsights/insights-ingress-go/validators"
 )
 
 var contentTypePat = regexp.MustCompile(`application/vnd\.redhat\.([a-z0-9-]+)\.([a-z0-9-]+).*`)
 
 func getServiceDescriptor(contentType string) (*validators.ServiceDescriptor, error) {
-	if contentType == "application/x-gzip; charset=binary" {
+	switch ctype := contentType; ctype {
+	case "application/x-gzip; charset=binary":
 		return &validators.ServiceDescriptor{
-			Service: "advisor",
+			Service:  "advisor",
 			Category: "upload",
 		}, nil
+	case "application/gzip":
+		return &validators.ServiceDescriptor{
+			Service:  "advisor",
+			Category: "upload",
+		}, nil
+	default:
+		m := contentTypePat.FindStringSubmatch(ctype)
+		if m == nil {
+			return nil, errors.New("Failed to match on Content-Type: " + ctype)
+		}
+		return &validators.ServiceDescriptor{
+			Service:  m[1],
+			Category: m[2],
+		}, nil
 	}
-	m := contentTypePat.FindStringSubmatch(contentType)
-	if m == nil {
-		return nil, errors.New("Failed to match on Content-Type: " + contentType)
-	}
-	return &validators.ServiceDescriptor{
-		Service:  m[1],
-		Category: m[2],
-	}, nil
 }
