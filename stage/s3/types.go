e1556f6fe2891437401870589f17066fa9fa9172
Jesse Jaggars <jjaggars@redhat.com>
2019-07-26 16:40:10 -0500

    removing simulation and cleaning up s3
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- stage/s3/types.go
+++ stage/s3/types.go
@@ -1,10 +1,16 @@
 package s3
 
-import "github.com/aws/aws-sdk-go/aws/session"
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
 
 // Stager provides the mechanism to stage a payload via aws S3
 type Stager struct {
 	Bucket   string
 	Rejected string
 	Sess     *session.Session
+	Uploader *s3manager.Uploader
+	Client   *s3.S3
 }
