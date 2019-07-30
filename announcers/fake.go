05ed4ff5a4c1d3b365bdae828578d442f160fb2b
Jesse Jaggars <jjaggars@redhat.com>
2019-07-30 07:11:38 -0500

    covering pipeline with tests
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- announcers/fake.go
+++ announcers/fake.go
@@ -8,23 +8,29 @@ import (
 
 // Fake is a fake announcer
 type Fake struct {
-	Event       *validators.Response
-	StatusEvent *Status
+	Event          *validators.Response
+	StatusEvent    *Status
+	AnnounceCalled bool
+	StatusCalled   bool
+	StopCalled     bool
 }
 
 // Announce does nothing
 func (f *Fake) Announce(e *validators.Response) {
+	f.AnnounceCalled = true
 	f.Event = e
 	l.Log.Info("Announce called", zap.String("request_id", e.RequestID))
 }
 
 // Status does nothing
 func (f *Fake) Status(e *Status) {
+	f.StatusCalled = true
 	f.StatusEvent = e
 	l.Log.Info("Announce called for Status", zap.String("request_id", e.RequestID))
 }
 
 // Stop does nothing
 func (f *Fake) Stop() {
+	f.StopCalled = true
 	l.Log.Info("Stop called")
 }
