9de6d5fd04853a7eddf38b0c2620720132f54c15
Stephen Adams <tsadams@gmail.com>
2019-08-27 14:26:44 -0400

    Add content type to debug logs
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- upload/upload.go
+++ upload/upload.go
@@ -102,6 +102,7 @@ func NewHandler(
 		}
 		observeSize(userAgent, fileHeader.Size)
 
+		l.Log.Debug("ContentType recieved from client", zap.String("ContentType", fileHeader.Header.Get("Content-Type")), logReqID)
 		serviceDescriptor, validationErr := getServiceDescriptor(fileHeader.Header.Get("Content-Type"))
 		if validationErr != nil {
 			logerr("Unable to validate", validationErr)
@@ -111,7 +112,7 @@ func NewHandler(
 
 		if fileHeader.Size > cfg.MaxSize {
 			l.Log.Info("File exceeds maximum file size for upload", zap.Int64("size", fileHeader.Size), zap.String("request_id", reqID))
-			w.WriteHeader(413)
+			w.WriteHeader(http.StatusRequestEntityTooLarge)
 			return
 		}
 
