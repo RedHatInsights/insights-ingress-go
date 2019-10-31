c5608a40186a0cb7e79f8b054734b80e25fd9153
Stephen Adams <tsadams@gmail.com>
2019-10-31 09:49:14 -0400

    Make logstream configurable

--- logger/logger.go
+++ logger/logger.go
@@ -83,13 +83,14 @@ func InitLogger() *logrus.Logger {
 	logconfig.SetDefault("LOG_LEVEL", "INFO")
 	logconfig.SetDefault("LOG_GROUP", "platform-dev")
 	logconfig.SetDefault("AWS_REGION", "us-east-1")
+	logconfig.SetDefault("LOG_STREAM", cfg.Hostname)
 	logconfig.SetEnvPrefix("INGRESS")
 	logconfig.AutomaticEnv()
 	key := logconfig.GetString("CW_AWS_ACCESS_KEY_ID")
 	secret := logconfig.GetString("CW_AWS_SECRET_ACCESS_KEY")
 	region := logconfig.GetString("AWS_REGION")
 	group := logconfig.GetString("LOG_GROUP")
-	stream := cfg.Hostname
+	stream := logconfig.GetString("LOG_STREAM")
 
 	switch logconfig.GetString("LOG_LEVEL") {
 	case "DEBUG":
