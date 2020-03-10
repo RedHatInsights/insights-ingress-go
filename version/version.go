023adb68d6ce4a04f06a0c15f8b4790f0a7bfbdc
Jesse Jaggars <jjaggars@redhat.com>
2020-03-10 12:52:43 -0400

    adding a metric with version information
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- version/version.go
+++ version/version.go
@@ -4,16 +4,33 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	cfg "github.com/redhatinsights/insights-ingress-go/config"
 	l "github.com/redhatinsights/insights-ingress-go/logger"
 )
 
-// GetVersion deals with calls to the version endpoint
-func GetVersion(w http.ResponseWriter, r *http.Request) {
-	v := &IngressVersion{
+func constructVersion() *IngressVersion {
+	return &IngressVersion{
 		Commit:  cfg.Get().OpenshiftBuildCommit,
 		Version: cfg.Get().Version,
 	}
+}
+
+func ExposeVersion() {
+	v := constructVersion()
+	versionMetric := promauto.NewGauge(prometheus.GaugeOpts{
+		Namespace:   "ingress",
+		Name:        "version",
+		Help:        "Version information for Ingress service.",
+		ConstLabels: prometheus.Labels{"commit": v.Commit, "version": v.Version},
+	})
+	versionMetric.Set(1.0)
+}
+
+// GetVersion deals with calls to the version endpoint
+func GetVersion(w http.ResponseWriter, r *http.Request) {
+	v := constructVersion()
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	jsonData, err := json.Marshal(v)
