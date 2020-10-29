881ccac7b501a72ea72b7786933610f4fc093d04
bholifie <bholifie@redhat.com>
2020-10-29 16:27:12 -0500

    factor out unit tests until working in eph env

--- pr_check.sh
+++ pr_check.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-export GO111MODULE="on"
-export GOPATH=/var/gopath
-ACG_CONFIG="$(pwd)/cdappconfig.json"  go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
-
-if [ $? != 0 ]; then
-    exit 1
-fi
+# source unit_test.sh
 
 # --------------------------------------------
 # Options that must be configured by app owner
