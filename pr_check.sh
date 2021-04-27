6ce670555a3fd43e4b5f81296fbb006cd70c89f9
Brandon Squizzato <bsquizza@redhat.com>
2021-04-27 11:05:45 -0400

    Download the right bootstrap

--- pr_check.sh
+++ pr_check.sh
@@ -13,10 +13,9 @@ IQE_FILTER_EXPRESSION=""
 
 
 # Install bonfire repo/initialize
-CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+#CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/97ab2481e3c08998201d3b8b7e1f497689eec5aa/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
-
-pip install crc-bonfire==v1.0.0b10
 
 source $CICD_ROOT/build.sh
 # uncomment when unit tests are present
