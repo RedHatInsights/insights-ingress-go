d73f104e2521b8fe79906315961738a99006d7a4
Brandon Squizzato <bsquizza@redhat.com>
2021-04-27 15:40:56 -0400

    Revert to master to try it out

--- pr_check.sh
+++ pr_check.sh
@@ -13,8 +13,7 @@ IQE_FILTER_EXPRESSION=""
 
 
 # Install bonfire repo/initialize
-#CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/local_enhancements/cicd
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
