99f70aa04465c01ef02b174b01a94d3053a17162
Brandon Squizzato <bsquizza@redhat.com>
2021-04-27 11:00:42 -0400

    Temp test for bonfire v1

--- pr_check.sh
+++ pr_check.sh
@@ -16,6 +16,8 @@ IQE_FILTER_EXPRESSION=""
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
+pip install crc-bonfire==v1.0.0b10
+
 source $CICD_ROOT/build.sh
 # uncomment when unit tests are present
 #source $APP_ROOT/unit_test.sh
