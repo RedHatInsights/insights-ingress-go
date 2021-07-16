a13c4acd860f1323e5a86b39adeda9c72e944770
Stephen Adams <tsadams@gmail.com>
2021-07-16 15:49:23 -0400

    [feat] use KafkaServers helper for clowder
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- config/config.go
+++ config/config.go
@@ -60,7 +60,7 @@ func Get() *IngressConfig {
 
 		options.SetDefault("WebPort", cfg.WebPort)
 		options.SetDefault("MetricsPort", cfg.MetricsPort)
-		options.SetDefault("KafkaBrokers", fmt.Sprintf("%s:%v", cfg.Kafka.Brokers[0].Hostname, *cfg.Kafka.Brokers[0].Port))
+		options.SetDefault("KafkaBrokers", clowder.KafkaServers)
 		options.SetDefault("MinioEndpoint", fmt.Sprintf("%s:%d", cfg.ObjectStore.Hostname, cfg.ObjectStore.Port))
 		options.SetDefault("MinioAccessKey", *cfg.ObjectStore.Buckets[0].AccessKey)
 		options.SetDefault("MinioSecretKey", *cfg.ObjectStore.Buckets[0].SecretKey)
