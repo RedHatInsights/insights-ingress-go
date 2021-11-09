4128e4d57cfd3f5ec55628d030bbd030a70338d1
Stephen Adams <tsadams@gmail.com>
2021-11-09 17:38:26 -0500

    add port to feature flag url
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- internal/featureflags/featureflags.go
+++ internal/featureflags/featureflags.go
@@ -2,6 +2,7 @@ package featureflags
 
 import (
 	"net/http"
+	"strconv"
 
 	unleash "github.com/Unleash/unleash-client-go/v3"
 	"github.com/redhatinsights/insights-ingress-go/internal/config"
@@ -13,7 +14,7 @@ var Client *unleash.Client
 func InitFFClient(cfg *config.IngressConfig) *unleash.Client {
 	Client, err := unleash.NewClient(
 		unleash.WithAppName("insights-ingress"),
-		unleash.WithUrl(cfg.FeatureFlagsConfig.FFHostname + "/api/"),
+		unleash.WithUrl(cfg.FeatureFlagsConfig.FFHostname + ":" + strconv.Itoa(cfg.FeatureFlagsConfig.FFPort) + "/api/"),
 		unleash.WithCustomHeaders(http.Header{"Authorization": {cfg.FeatureFlagsConfig.FFToken}}),
 	)
 
