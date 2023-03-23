6adb1c72da547a53bee0e0e23f96909d5e734063
dehort <dehort@redhat.com>
2023-03-23 10:05:52 -0500

    Remove service specific topic logic (#312)
    * Remove service specific topic logic
    
    * Remove the "hidden" mapping of "openshift" service to "buckit" topic
    
    * More cleanup
    
    * Rename config option from VALIDTOPICS to VALID_UPLOAD_TYPES
    
    * Make sure to pull the kafka topic name from clowder config
    Remove debug logging
    
    * Ugh.  Fixed.
    
    * Remove unneeded kafka topics
    
    * Remove more legacy details from the readme
    
    ---------
    
    Co-authored-by: Tahmid Efaz <tefaz@redhat.com>

--- internal/validators/kafka/kafka.go
+++ internal/validators/kafka/kafka.go
@@ -3,7 +3,6 @@ package kafka
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 
 	"github.com/redhatinsights/insights-ingress-go/internal/config"
 	l "github.com/redhatinsights/insights-ingress-go/internal/logger"
@@ -12,17 +11,9 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var tdMapping map[string]string
-
-func init() {
-	tdMapping = make(map[string]string)
-	tdMapping["unit2"] = "unit"
-	tdMapping["openshift"] = "buckit"
-}
-
 // Validator posts requests to topics for validation
 type Validator struct {
-	ValidationProducerMapping map[string]chan validators.ValidationMessage
+	ValidationProducerChannel chan validators.ValidationMessage
 	KafkaBrokers              []string
 	KafkaGroupID              string
 	Username                  string
@@ -30,6 +21,7 @@ type Validator struct {
 	CA                        string
 	SASLMechanism             string
 	KafkaSecurityProtocol     string
+	validUploadTypes          map[string]bool
 }
 
 // Config configures a new Kafka Validator
@@ -46,9 +38,9 @@ type Config struct {
 }
 
 // New constructs and initializes a new Kafka Validator
-func New(cfg *Config, topics ...string) *Validator {
+func New(cfg *Config, validServices ...string) *Validator {
 	kv := &Validator{
-		ValidationProducerMapping: make(map[string]chan validators.ValidationMessage),
+		ValidationProducerChannel: make(chan validators.ValidationMessage),
 		KafkaBrokers:              cfg.Brokers,
 		KafkaGroupID:              cfg.GroupID,
 		KafkaSecurityProtocol:     cfg.KafkaSecurityProtocol,
@@ -67,13 +59,11 @@ func New(cfg *Config, topics ...string) *Validator {
 		kv.SASLMechanism = cfg.SASLMechanism
 	}
 
-	// ensure the announce topic is added and valid
-	topics = append(topics, "announce")
+	kv.validUploadTypes = buildValidUploadTypeMap(validServices)
 
-	for _, topic := range topics {
-		topic = config.GetTopic(fmt.Sprintf("platform.upload.%s", topic))
-		kv.addProducer(topic)
-	}
+	announceTopic := config.Get().KafkaConfig.KafkaAnnounceTopic
+
+	kv.addProducer(announceTopic)
 
 	return kv
 }
@@ -85,11 +75,8 @@ func (kv *Validator) Validate(vr *validators.Request) {
 		l.Log.WithFields(logrus.Fields{"error": err}).Error("failed to marshal json")
 		return
 	}
-	topic := serviceToTopic(vr.Service)
-	topic = fmt.Sprintf("platform.upload.%s", topic)
 	announceTopic := config.Get().KafkaConfig.KafkaAnnounceTopic
-	realizedTopicName := config.GetTopic(topic)
-	l.Log.WithFields(logrus.Fields{"data": data, "topic": realizedTopicName}).Debug("Posting data to topic")
+	l.Log.WithFields(logrus.Fields{"data": data, "topic": announceTopic}).Debug("Posting data to topic")
 	message := validators.ValidationMessage{
 		Message: data,
 		Headers: map[string]string{
@@ -99,14 +86,9 @@ func (kv *Validator) Validate(vr *validators.Request) {
 	if vr.Metadata.QueueKey != "" {
 		message.Key = []byte(vr.Metadata.QueueKey)
 	}
-	switch account := vr.Account; account {
-	case "":
-		kv.ValidationProducerMapping[config.GetTopic(announceTopic)] <- message
-		incMessageProduced(vr.Service)
-	default:
-		kv.ValidationProducerMapping[realizedTopicName] <- message
-		kv.ValidationProducerMapping[config.GetTopic(announceTopic)] <- message
-	}
+
+	kv.ValidationProducerChannel <- message
+	incMessageProduced(vr.Service)
 }
 
 func (kv *Validator) addProducer(topic string) {
@@ -120,24 +102,28 @@ func (kv *Validator) addProducer(topic string) {
 		KafkaSecurityProtocol: kv.KafkaSecurityProtocol,
 		SASLMechanism:         kv.SASLMechanism,
 	})
-	kv.ValidationProducerMapping[topic] = ch
+	kv.ValidationProducerChannel = ch
 }
 
 // ValidateService ensures that a service maps to a real topic
 func (kv *Validator) ValidateService(service *validators.ServiceDescriptor) error {
-	topic := serviceToTopic(service.Service)
-	for _, validTopic := range config.Get().KafkaConfig.ValidTopics {
-		if validTopic == topic {
-			return nil
-		}
+
+	_, isValidUploadType := kv.validUploadTypes[service.Service]
+
+	if isValidUploadType {
+		return nil
 	}
-	return errors.New("Validation topic is invalid: " + topic)
+
+	return errors.New("Upload type is not supported: " + service.Service)
 }
 
-func serviceToTopic(service string) string {
-	topic := tdMapping[service]
-	if topic != "" {
-		return topic
+func buildValidUploadTypeMap(validUploadTypeList []string) map[string]bool {
+
+	validUploadTypes := make(map[string]bool)
+
+	for _, service := range validUploadTypeList {
+		validUploadTypes[service] = true
 	}
-	return fmt.Sprintf("%s", service)
+
+	return validUploadTypes
 }
