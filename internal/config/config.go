05dbb07b6a8ed7df6436550ed37eab1a6e02e7f4
Derek Horton <dehort@redhat.com>
2023-03-27 11:24:21 -0500

    Fix spelling of announce topic config name

--- internal/config/config.go
+++ internal/config/config.go
@@ -96,7 +96,7 @@ func Get() *IngressConfig {
 	options.SetDefault("KafkaGroupID", "ingress")
 	options.SetDefault("KafkaDeliveryReports", true)
 	options.SetDefault("KafkaTrackerTopic", "platform.payload-status")
-	options.SetDefault("KafakAnnounceTopic", "platform.upload.announce")
+	options.SetDefault("KafkaAnnounceTopic", "platform.upload.announce")
 	options.SetDefault("KafkaSecurityProtocol", "PLAINTEXT")
 
 	// Global defaults
@@ -216,7 +216,7 @@ func Get() *IngressConfig {
 			KafkaGroupID:          options.GetString("KafkaGroupID"),
 			KafkaTrackerTopic:     options.GetString("KafkaTrackerTopic"),
 			KafkaDeliveryReports:  options.GetBool("KafkaDeliveryReports"),
-			KafkaAnnounceTopic:    options.GetString("KafakAnnounceTopic"),
+			KafkaAnnounceTopic:    options.GetString("KafkaAnnounceTopic"),
 			ValidTopics:           strings.Split(options.GetString("ValidTopics"), ","),
 			KafkaSecurityProtocol: options.GetString("KafkaSecurityProtocol"),
 		},
