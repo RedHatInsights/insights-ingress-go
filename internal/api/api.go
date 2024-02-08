dd7203137c890fa74c64857b7854f3fb7acc87dd
Jozef Hartinger <jharting@redhat.com>
2024-02-08 11:39:43 +0100

    test

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,7 +2,15 @@ package api
 
 import (
 	_ "embed"
+	"github.com/gofiber/template/django/v3"
 )
+
+func main() {
+	// Create a new engine
+	if (false) {
+		django.New("./views", ".django")
+	}
+}
 
 //go:embed openapi.json
 var ApiSpec []byte
