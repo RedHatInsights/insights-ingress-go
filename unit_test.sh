e5bc370d9fe6a587f01d5352d12b428d1689c206
Tahmid Efaz <tefaz@redhat.com>
2024-04-18 11:19:50 -0400

    testing: checking which version is available through jenkins

--- unit_test.sh
+++ unit_test.sh
@@ -4,6 +4,7 @@
 export GO111MODULE="on"
 export GOPATH="$(pwd)/gopath"
 export GOROOT="/opt/go/1.21.9"
+echo | ls /opt/go
 export PATH="${GOROOT}/bin:${PATH}"
 ACG_CONFIG="$(pwd)/cdappconfig.json"  go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
 
